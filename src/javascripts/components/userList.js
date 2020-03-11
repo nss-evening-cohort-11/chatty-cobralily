0a6fdac20c08ec13dddcbed15a2aeea6ecf75e3b
delindell <daviselindell@gmail.com>
2020-03-10 23:48:18 -0500

    wip

---
+++ src/javascripts/components/userList.js
@@ -0,0 +1,8 @@
+import userData from '../helpers/data/userData';
+
+const createUserList = (messages) => {
+  const users = userData.getUsers();
+  messages.forEach((message) => {
+    const messageUsers = users.find((x) => x.id === message.userId);
+  });
+}
