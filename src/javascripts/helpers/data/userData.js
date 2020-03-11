0a7eac3ee495a8e2f4382c319c19519c51aa4918
Zac Crumpton <zaccrumpton@Zacs-MacBook-Pro.local>
2020-03-10 21:08:57 -0500

    wip

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -4,3 +4,19 @@ const users = [
   { id: 'user3', name: 'Monique' },
   { id: 'user4', name: 'Zac' }
 ];
+
+const setSelectedUser = (userId) => {
+  const selectedUserId = userId;
+  console.log('selectedUserId', selectedUserId);
+};
+
+const getSelectedUser = () => {
+  const findUser = users.find((x) => x.id === selectedUserId)
+  return findUser;
+}
+
+const getUsers = () => {
+  return users;
+};
+
+export default { setSelectedUser, getSelectedUser, getUsers }
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,9 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import radio from './components/radio';
+
+const init = () => {
+  radio.buildRadioButtons();
+};
+
+init();
