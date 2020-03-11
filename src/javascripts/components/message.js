e0213590fa9b884b5b6dbea60d40b3270959e943
Beth <bethhnielsen56@gmail.com>
2020-03-10 21:25:07 -0500

    wip

--- src/javascripts/components/message.js
+++ src/javascripts/components/message.js
@@ -6,11 +6,14 @@ const messageBuilder = () => {
   const textInfo = message.getMessages();
   textInfo.forEach((text) => {
     domString += '<div class="bg-white border-top row w-75 mx-auto m-1 p-2">';
-    domString += '<div>';
+    domString += '<div class="col-10">';
     domString += '<h5>Name Example</h5>';
-    domString += '</div>';
-    domString += '<div class="ml-3">';
+    domString += '<div>';
     domString += `<p>${text.message}</p>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '<div>';
+    domString += '<button class="btn btn-danger mt-3">Delete <i class="fas fa-minus-circle"></i></button>';
     domString += '</div>';
     domString += '</div>';
   });
