e1e33be59a73cdba0c7eb2aa336c0a2427c7f062
Beth <bethhnielsen56@gmail.com>
2020-03-12 23:15:09 -0500

    wip

--- src/javascripts/components/message.js
+++ src/javascripts/components/message.js
@@ -5,7 +5,7 @@ const messageBuilder = () => {
   let domString = '';
   const textInfo = message.getMessages();
   textInfo.forEach((text) => {
-    domString += '<div class="bg-white border-top row w-75 mx-auto m-1 p-2">';
+    domString += '<div id="message" class="message bg-white border-top row w-75 mx-auto m-1 p-2">';
     domString += '<div class="col-10">';
     domString += '<h5>Name Example</h5>';
     domString += '<div>';
@@ -13,11 +13,20 @@ const messageBuilder = () => {
     domString += '</div>';
     domString += '</div>';
     domString += '<div>';
-    domString += '<button class="btn btn-danger mt-3">Delete <i class="fas fa-minus-circle"></i></button>';
+    domString += `<button id="${text.id}" class="deleteBtn btn btn-danger">Delete <i class="fas fa-minus-circle"></i></button>`;
     domString += '</div>';
     domString += '</div>';
   });
   utils.printToDom('messageContainer', domString);
+  const deleteMessageEvent = (e) => {
+    textInfo.forEach((deleteText) => {
+      const deleteId = e.target.closest('.message').id;
+      const textPosition = deleteText.findIndex((p) => p.id === deleteId);
+      deleteText.splice(textPosition, 1);
+      messageBuilder(deleteText);
+    });
+  };
+  $('.deleteBtn').click(deleteMessageEvent);
 };
 
 export default { messageBuilder };
