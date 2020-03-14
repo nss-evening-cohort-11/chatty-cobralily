f88738e73096895049c4acb03f7915cb2c52b248
Beth <bethhnielsen56@gmail.com>
2020-03-14 13:08:31 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,7 +5,7 @@ import navbar from './components/navbar/navbar';
 import messages from './components/message/message';
 import radio from './components/radio/radio';
 import userData from './helpers/data/userData';
-import 'moment';
+
 
 const init = () => {
   radio.buildRadioButtons(userData.getUsers);
