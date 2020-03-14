0327ebd4b1483b47b105608d5fcb6941b2074691
delindell <daviselindell@gmail.com>
2020-03-14 14:46:46 -0500

    trying again

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -26,7 +26,7 @@ const buildNavbar = () => {
         const newMessage = {
           id: `message${messages.length + 1}`,
           message: $('#message-field').val(),
-          timeStamp: moment().format('MMM Do YY'),
+          timeStamp: moment().format('MMM Do YYYY h:mm a'),
           userId: userData.getSelectedUser(),
         };
         messages.push(newMessage);
