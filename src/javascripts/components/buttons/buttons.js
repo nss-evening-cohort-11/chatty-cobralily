1e0577a231083fa4ddc003e44ce9e4919c5ada23
Beth <bethhnielsen56@gmail.com>
2020-03-14 23:00:21 -0500

    increase text size working

--- src/javascripts/components/buttons/buttons.js
+++ src/javascripts/components/buttons/buttons.js
@@ -3,9 +3,9 @@ import utils from '../../helpers/utils';
 
 const settings = () => {
   let domString = '';
-  domString += '<div class="btn-group">';
-  domString += '<button type="button" class="btn btn-outline-success dropdown-toggle btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</button>';
-  domString += '<div class="dropdown-menu">';
+  domString += '<div class="btn-group bg-light float-right m-2">';
+  domString += '<button type="button" class="btn btn-outline-success dropdown-toggle px-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</button>';
+  domString += '<div class="dropdown-menu ml-3">';
   domString += '<a class="dropdown-item" id="largeText" href="#">Increase Text</a>';
   domString += '<a class="dropdown-item" id="standardText" href="#">Standard Text</a>';
   domString += '<a class="dropdown-item" href="#">Light Mode</a>';
