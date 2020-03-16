dbfaad3dc5ca96bed93d3a574001930bf4b3cdf6
delindell <daviselindell@gmail.com>
2020-03-15 20:36:35 -0500

    added seed Timestamps

--- src/javascripts/helpers/data/messageData.js
+++ src/javascripts/helpers/data/messageData.js
@@ -3,31 +3,31 @@ const messages = [
     id: 'message1',
     message: 'Hello everyone! Welcome to hell',
     userId: 'user1',
-    timeStamp: '',
+    timeStamp: 'March 15th 2020, 6:26 pm',
   },
   {
     id: 'message2',
     message: 'Yall are weirdos!',
     userId: 'user3',
-    timeStamp: '',
+    timeStamp: 'March 15th 2020, 6:26 pm',
   },
   {
     id: 'message3',
     message: 'Hey! I think everyone is awesome!',
     userId: 'user2',
-    timeStamp: '',
+    timeStamp: 'March 15th 2020, 6:26 pm',
   },
   {
     id: 'message4',
     message: 'Thanks for saying that my friend.',
     userId: 'user4',
-    timeStamp: '',
+    timeStamp: 'March 15th 2020, 6:26 pm',
   },
   {
     id: 'message5',
     message: 'Hey buddy, what is up?',
     userId: 'user4',
-    timeStamp: '',
+    timeStamp: 'March 15th 2020, 6:26 pm',
   },
 ];
 
