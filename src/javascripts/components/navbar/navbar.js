eab9e1930910f3b1d468a3a8fa81b0dd654a278c
Beth <bethhnielsen56@gmail.com>
2020-03-15 20:54:22 -0500

    wip

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -3,8 +3,21 @@ import utils from '../../helpers/utils';
 import messageData from '../../helpers/data/messageData';
 import message from '../message/message';
 // import radio from '../radio';
+import gifData from '../../helpers/data/gifData';
 import userData from '../../helpers/data/userData';
 
+const requestGifs = () => {
+  const inputGif = $('#gifInput').val();
+  gifData.getGifs(inputGif)
+    .then((results) => {
+      const gifs = results.data.data;
+      let domString = '';
+      domString += `<img src="${gifs[0].url}"/>`;
+      utils.printToDom('gifContainer', domString);
+      console.error('results', gifs[0], inputGif);
+    })
+    .catch((error) => console.error('error', error));
+};
 
 const buildNavbar = () => {
   let domString = '';
@@ -14,10 +27,15 @@ const buildNavbar = () => {
   domString += '<form class="form-inline my-2 my-lg-0 w-75">';
   domString += '<input class="form-control w-75 mx-auto form-control-lg" type="text" id="message-field" placeholder="Enter message" aria-label="Search">';
   domString += '</form>';
-  domString += '<button class="btn btn-outline-success btn-lg ml-5" id="clear-all-button">Clear All Messages</button>';
+  domString += '<input class="form-control" id="gifInput" placeholder="Add Gif"></input>';
+  domString += '<button class="btn btn-outline-success btn" id="addGif">Add Gif</button>';
+  domString += '<button class="btn btn-outline-success btn" id="clear-all-button">Clear All Messages</button>';
   domString += '</nav>';
   domString += '</div>';
   utils.printToDom('nav', domString);
+
+  $('#addGif').click(requestGifs);
+
   $('#message-field').keypress((e) => {
     if (e.keyCode === 13) {
       e.preventDefault();
