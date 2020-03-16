bd4ca55b272c2c052fbc810e4a953570e638727e
Zac Crumpton <zaccrumpton@zacs-mbp.attlocal.net>
2020-03-16 03:01:10 -0500

    changed some css for themes and added logo

--- src/javascripts/components/buttons/buttons.js
+++ src/javascripts/components/buttons/buttons.js
@@ -3,8 +3,8 @@ import utils from '../../helpers/utils';
 
 const settings = () => {
   let domString = '';
-  domString += '<div id="settingsContainer" class="btn-group bg-light float-right m-2">';
-  domString += '<button type="button" id="themeSelectorBtns" class="btn btn-outline-success dropdown-toggle px-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</button>';
+  domString += '<div id="settingsContainer" class="btn-group bg-dark float-right m-2">';
+  domString += '<button type="button" id="themeSelectorBtns" class="btn btn-primary dropdown-toggle px-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</button>';
   domString += '<div class="dropdown-menu ml-3">';
   domString += '<a class="dropdown-item" id="largeText" href="#">Increase Text</a>';
   domString += '<a class="dropdown-item" id="standardText" href="#">Standard Text</a>';
@@ -14,16 +14,14 @@ const settings = () => {
   domString += '</div>';
   utils.printToDom('settings', domString);
   const fontIncrease = () => {
-    $('.fontIncrease').css({ 'font-size': '200%' });
+    $('body').css({ 'font-size': '200%' });
+    $('button').css({ 'font-size': '25px' });
   };
   const font = () => {
-    $('.standard').css({ 'font-size': '' });
+    $('body').css({ 'font-size': '' });
+    $('button').css({ 'font-size': '' });
   };
-  $('#largeText').click(fontIncrease);
-  $('#standardText').click(font);
-};
-$(document).ready(() => {
-  $('#dark-mode').click(() => {
+  const darkTheme = () => {
     $('body').css('background-color', 'black');
     $('a').css('color', 'white');
     $('form').css('color', 'white');
@@ -38,19 +36,19 @@ $(document).ready(() => {
     $('label').css('color', 'white');
     $('.dropdown-menu').css('background-color', 'black');
     $('.dropdown-item').css('color', 'white');
-    // $('div').css('background-color', 'black');
-    // $('div').css('color', 'white');
     $('col').css('background-color', 'black');
     $('col').css('color', 'white');
     $('col').css('border-color', 'black');
-    $('button').css('background-color', 'black');
+    $('#clear-all-button').removeClass('btn-primary');
+    $('#clear-all-button').addClass('btn-danger');
+    $('#themeSelectorBtns').removeClass('btn-primary');
+    $('#themeSelectorBtns').addClass('btn-danger');
     $('button').css('color', 'white');
-    $('border').css('background-color', 'black');
-    $('border').css('color', 'white');
-  });
-});
-$(document).ready(() => {
-  $('#light-mode').click(() => {
+    $('.message').removeClass('bg-white');
+    $('.message').css('background-color', 'slategrey');
+    $('.message').css('color', 'white');
+  };
+  const lightTheme = () => {
     $('body').css('background-color', '');
     $('a').css('background-color', '');
     $('a').css('color', '');
@@ -60,6 +58,7 @@ $(document).ready(() => {
     $('nav').removeClass('bg-dark');
     $('nav').addClass('navbar-light');
     $('nav').addClass('bg-light');
+    $('nav').css('color', 'black');
     $('#settings').removeClass('bg-dark');
     $('#settings').addClass('bg-light');
     $('#userRadioBtnContainer').removeClass('bg-dark');
@@ -70,12 +69,20 @@ $(document).ready(() => {
     $('col').css('background-color', '');
     $('col').css('color', '');
     $('col').css('border-color', '');
-    $('button').css('background-color', '');
+    $('#clear-all-button').removeClass('btn-danger');
+    $('#clear-all-button').addClass('btn-primary');
+    $('#themeSelectorBtns').removeClass('btn-danger');
+    $('#themeSelectorBtns').addClass('btn-primary');
     $('button').css('color', '');
-    $('border').css('background-color', '');
-    $('border').css('color', '');
-  });
-});
+    $('.message').addClass('bg-white');
+    $('.message').css('color', '');
+  };
+
+  $('#largeText').click(fontIncrease);
+  $('#standardText').click(font);
+  $('#dark-mode').click(darkTheme);
+  $('#light-mode').click(lightTheme);
+};
 
 const getButtons = () => settings;
 
--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -1,8 +1,8 @@
 const users = [
-  { id: 'user1', name: 'Beth', photo: 'https://vignette.wikia.nocookie.net/gumby/images/c/c7/Gumby_at_his_Desk.jpg/revision/latest/scale-to-width-down/340?cb=20140521010057' },
-  { id: 'user2', name: 'Davis', photo: 'https://vignette.wikia.nocookie.net/gumby/images/c/c7/Gumby_at_his_Desk.jpg/revision/latest/scale-to-width-down/340?cb=20140521010057' },
-  { id: 'user3', name: 'Monique', photo: 'https://vignette.wikia.nocookie.net/gumby/images/c/c7/Gumby_at_his_Desk.jpg/revision/latest/scale-to-width-down/340?cb=20140521010057' },
-  { id: 'user4', name: 'Zac', photo: 'https://vignette.wikia.nocookie.net/gumby/images/c/c7/Gumby_at_his_Desk.jpg/revision/latest/scale-to-width-down/340?cb=20140521010057' },
+  { id: 'user1', name: 'Beth', photo: 'https://ca.slack-edge.com/T03F2SDTJ-USB74J3NG-bd9492cd1c78-512' },
+  { id: 'user2', name: 'Davis', photo: 'https://ca.slack-edge.com/T03F2SDTJ-UQUEELZA9-b05c885e34a5-512' },
+  { id: 'user3', name: 'Monique', photo: 'https://ca.slack-edge.com/T03F2SDTJ-UQWL7G33Q-gfa902f0f64d-512' },
+  { id: 'user4', name: 'Zac', photo: 'https://ca.slack-edge.com/T03F2SDTJ-USF25R1P1-296d07f50e2b-512' },
 ];
 
 let selectedUserId = '';
